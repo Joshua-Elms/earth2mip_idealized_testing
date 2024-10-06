c7c251ca4e2fbcf646e91fe551cf1914134715dd
Joshua-Elms <joshuaelms@lbl.gov>
2024-10-05 20:50:42 -0700

    incomplete modifications to ankur's run script and setup

---
+++ submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C 'cpu'
+#SBATCH -q regular
+#SBATCH -J HENS_test
+#SBATCH --mail-user=jmelms@iu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:30:00
+#SBATCH -A m1517
+#SBATCH -o job_logs/%j.out
+
+# Your existing srun command with the selected date
+srun -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 bash -c "source set_74ch_vars.sh; python -m earth2mip.inference_ensemble configs/test_n1.json"
