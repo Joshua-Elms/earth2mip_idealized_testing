25e00cc725e28eba8b9ee71c84bd0aa5588a148c
Joshua-Elms <joshuaelms@lbl.gov>
2024-10-24 21:28:01 -0700

    working on first run still. pydantic and interactive errors.

--- set_74ch_vars.sh
+++ set_74ch_vars.sh
@@ -1,4 +1,4 @@
-export MODEL_REGISTRY=/global/cfs/cdirs/m1517/cascade/joshelms/fcn_model_registry
+export MODEL_REGISTRY=/global/cfs/cdirs/m1517/cascade/joshelms/fcn_model_registry/sfno_74ch
 # export MASTER_ADDR=$(hostname) # HENS
 # export MASTER_PORT=29500 # HENS
 # export ERA5_HDF5=/pscratch/sd/p/pharring/74var-6hourly/staging/ # HENS
--- submit_job.sh
+++ submit_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -C 'cpu'
+#SBATCH -C 'gpu&hbm80g'
 #SBATCH -q regular
 #SBATCH -J HENS_test
 #SBATCH --mail-user=jmelms@iu.edu
@@ -10,5 +10,4 @@
 #SBATCH -o job_logs/%j.out
 
 # Your existing srun command with the selected date
-export MODEL_REGISTRY="/global/cfs/cdirs/m1517/cascade/joshelms/fcn_model_registry/sfno_74ch/sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed102"
-srun -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 bash -c "source set_74ch_vars.sh; python -m earth2mip.inference_ensemble configs/test_n1.json"
+srun -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 bash -c "source set_74ch_vars.sh; pip list; python -m earth2mip.inference_ensemble configs/test_n1.json"
