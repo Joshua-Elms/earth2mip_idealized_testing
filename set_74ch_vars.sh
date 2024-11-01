55cb2718cb43ef9aea6a9567ffeb05263cca524a
Joshua-Elms <joshuaelms@lbl.gov>
2024-10-31 21:00:17 -0700

    still updating HENS code, need HEAT_INDEX_...

--- set_74ch_vars.sh
+++ set_74ch_vars.sh
@@ -1,5 +1,6 @@
 export MODEL_REGISTRY=/global/cfs/cdirs/m1517/cascade/joshelms/fcn_model_registry/sfno_74ch
-# export MASTER_ADDR=$(hostname) # HENS
-# export MASTER_PORT=29500 # HENS
-# export ERA5_HDF5=/pscratch/sd/p/pharring/74var-6hourly/staging/ # HENS
-# export DETERMINISTIC_RMSE=/pscratch/sd/a/amahesh/hens/optimal_perturbation_targets/means/d2m_sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed16.nc # ?
+export ERA5_HDF5=/pscratch/sd/p/pharring/74var-6hourly/staging/ # HENS
+export MASTER_ADDR=$(hostname) # HENS
+export MASTER_PORT=29500 # HENS
+export DETERMINISTIC_RMSE=/pscratch/sd/a/amahesh/hens/optimal_perturbation_targets/means/d2m_sfno_linear_74chq_sc2_layers8_edim620_wstgl2-epoch70_seed16.nc # ?
+export HEAT_INDEX_LOOKUP_TABLE=/pscratch/sd/a/amahesh/hens/prod_heat_index_lookup.zarr
--- submit_job.sh
+++ submit_job.sh
@@ -10,5 +10,5 @@
 #SBATCH -o job_logs/%j.out
 
 # Your existing srun command with the selected date
-srun -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 --env=PYTHONUSERBASE=/global/homes/j/joshelms/supplemental_packages/pydantic_supplement_modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint bash -c "source set_74ch_vars.sh; python -m earth2mip.inference_ensemble configs/test_n1.json"
+srun --gpus-per-node=1 -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 --env=PYTHONUSERBASE=/global/homes/j/joshelms/supplemental_packages/pydantic_supplement_modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint bash -c "source set_74ch_vars.sh; python -m earth2mip.inference_ensemble configs/test_n1.json"
 # srun -u shifter --image=amahesh19/modulus-makani:0.1.0-torch_patch-23.11-multicheckpoint --module=gpu,nccl-2.18 bash -c "source set_74ch_vars.sh; pip list; python -m earth2mip.inference_ensemble configs/test_n1.json"
